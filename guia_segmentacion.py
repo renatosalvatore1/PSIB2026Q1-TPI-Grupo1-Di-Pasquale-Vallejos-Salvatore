4e0d359d11d3fda3072e585e03eb6e7bc04cb8bc
Ludmila Vallejos <luvallejos@itba.edu.ar>
2026-04-10 14:47:14 -0300

    cargué eritrocitos desde url

--- guia_segmentacion.py
+++ guia_segmentacion.py
@@ -1,1 +1,15 @@
-#hola soy nato
+#importo imagen desde url
+import cv2
+import urllib.request
+import numpy as np
+
+url = "https://www.pictoeduca.com/uploads/2017/07/eritrocitos.jpg" #erictrocitos
+req = urllib.request.urlopen(url)
+arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
+image = cv2.imdecode(arr, -1)
+
+cv2.imshow("Image", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
