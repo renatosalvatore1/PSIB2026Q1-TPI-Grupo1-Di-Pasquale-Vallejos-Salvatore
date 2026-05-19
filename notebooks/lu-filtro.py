cd7e083f8632f254aa09e8013ce0bace8422f848
Ludmila Vallejos <luvallejos@itba.edu.ar>
2026-05-19 18:46:15 -0300

    filtradoNL-means

--- notebooks/lu-filtro.py
+++ notebooks/lu-filtro.py
@@ -1,14 +1,29 @@
 import nibabel as nib
+import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+from skimage.restoration import denoise_nl_means, estimate_sigma
+from skimage import img_as_float
+'''
+fun fact: nibabel suele cargar volúmenes tridimensionales, .get_data devuelve un arreglo 3D indexado
+podríamos también cargar el dicom con la libreria pydicom, que lee los archivos corte por corte (2D)
+'''
 
 #analogo al WaitKey en cv2
 def al_presionar_tecla(event):
     plt.close()
 
+#_____________________________________________________________________________
+#CARGA DEL FRAME DESDE DICOM Y NORMALIZACIÓN
+#_____________________________________________________________________________
+
 data = nib.load('./data/sub-KA02/anat/sub-KA02_run-02_T1w.nii.gz').get_fdata()
 frame = data[:,:,89]
 frame_n = cv2.normalize(frame, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U) #dicom trabaja con 16bits, cv2 con 8
+
+#_____________________________________________________________________________
+#ANÁLISIS EN BASE AL HISTOGRAMA
+#_____________________________________________________________________________
 hist = cv2.calcHist([frame_n],[0],None,[256],[0,256])
 
 fig,axs=plt.subplots(1,2, figsize=(10,5))
@@ -30,6 +45,9 @@ las zonas claras (cola larga hacia zonas de mayor intensidad y agrupamiento dens
 Para tratar estos ruidos se propone aplicar un filtro Gaussiano y un filtro mediana, respectivamente.
 '''
 
+#_____________________________________________________________________________
+#APLICACIÓN DE FILTROS MEDIANA Y GAUSSIANO
+#_____________________________________________________________________________
 mediana = cv2.medianBlur(frame_n,ksize=5)
 gaussiano = cv2.GaussianBlur(frame_n, ksize=(5, 5), sigmaX=2, sigmaY=2, borderType=cv2.BORDER_CONSTANT)
 hist_m = cv2.calcHist([mediana],[0],None,[256],[0,256])
@@ -75,4 +93,42 @@ axs[2].set_xlim([0,256])
 plt.title('Frame 89 + Filtrado')
 plt.tight_layout()
 fig.canvas.mpl_connect('key_press_event', al_presionar_tecla) #waitKey
+plt.show()
+
+
+#_____________________________________________________________________________
+#APLICACIÓN DE NL-MEANS
+#_____________________________________________________________________________
+#normalizar en el rango [0,1] para skimage, en vez de [0,255] o unidades Hounsfield
+min, max = frame.min(), frame.max()
+image = (frame - min)/(max - min)
+
+#estimar el ruido sigma en el fondo: hay que asegurarse de que la esquina corresponda a una zona sin anatomía
+esquina = image[0:40, 0:40]
+mean_esquina = np.mean(esquina)
+sigma = mean_esquina / np.sqrt(np.pi/2)
+
+#correción de sesgo
+squared = image**2 - 2*(sigma**2)
+corrected = np.sqrt(np.maximum(squared,0))
+
+h = 0.7 * sigma #filtrado Non-Local Means
+
+filtrada = denoise_nl_means(corrected,h=h,sigma=sigma,fast_mode=True,patch_size=5,patch_distance=11,channel_axis=None)
+    #patch_size=5, patch_distance=11 estandar para mri estructural T1,T2.
+    #subir si la resolucion es muy alta o ruido es extremadamente grueso a 7 y 15 respectivamente
+    #a costo de aumento de tiempo de cómputo
+
+final = (filtrada * (max - min)) + min#restaurar rango dinámico original si es necesario
+
+fig,axs = plt.subplots(1,2,figsize=(18,6))
+axs[0].imshow(frame, cmap='gray')
+axs[0].set_title('Original')
+axs[0].axis('off')
+
+axs[1].imshow(final, cmap='gray')
+axs[1].set_title('Filtrado con NL-means')
+axs[1].axis('off')
+
+plt.tight_layout()
 plt.show()
